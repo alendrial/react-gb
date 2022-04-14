ae877983b393cff9534102d16f68b1aa14277cc6
Alendrial <mashkinmv94@gmail.com>
2022-04-14 18:49:34 +0700

    lesson-2-after

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react"
-//import "./App.css"
 import './App.scss'
 import { Form } from "./Components/FormFunc/Form"
-// import { Parent } from "./Components/FormFunc/Parent"
+
 
 export const App = () => {
   return (
     <div className="form" style={{ marginTop: "20px" }}>
       <Form />
-      {/* <Parent /> */}
     </div>
   )
 }
--- src/Components/FormFunc/Message.jsx
+++ src/Components/FormFunc/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Message = (props) => {
   
-  return (
+  return 
     <>
       <p className="author">{props.message.author}</p>
       <p className="text">{props.message.message}</p>
