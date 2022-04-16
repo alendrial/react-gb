d8bb62065ae2c71b2ed2ef20ea279403f8c4fdec
Alendrial <mashkinmv94@gmail.com>
2022-04-16 21:20:26 +0700

    Version 2 jest

--- src/Components/FormFunc/Button/Button.jsx
+++ src/Components/FormFunc/Button/Button.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 export const Button = (props) => {
   return (
-    <button onClick={props.click} disabled={props.disabled}>
-      {props.name}
+    <button
+      onClick={props.click}
+      disabled={props.disabled}
+      style={{ backgroundColor: "#4cc49c" }}
+    >
+      Send message!
     </button>
   );
 };
-
 
 Button.propTypes = {
   disabled: PropTypes.bool,
   name: PropTypes.string,
   onClick: PropTypes.func,
-}
+};
--- src/Components/FormFunc/Button/Button.test.jsx
+++ src/Components/FormFunc/Button/Button.test.jsx
@@ -1,8 +1,52 @@
-import Button from './Button'
-import { render, screen } from '@testing-library/react'
+import React from 'react';
+import { Button } from './Button'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { toBeDisabled } from '@testing-library/jest-dom/dist/matchers';
+import userEvent from '@testing-library/user-event';
+import { waitFor } from '@storybook/testing-library';
 
-describe('Button', () => {
+describe('Button', (props) => {
     it('render component', () => {
-
+        render(<Button />)
     })
+    it('render with snapshot', () => {
+        const { asFragment } = render(<Button />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+    it('render with text', () => {
+        render(<Button />)
+        expect(screen.getByText(/Send message!/)).toBeInTheDocument()
+    })
+    it('render multiple components', () => {
+        render(
+        <>
+        <Button />
+        <Button />
+        </>)
+        expect(screen.queryAllByRole('button').length).toBe(2)
+    })
+    it('button is disabled', () => {
+        render(<Button disabled />)
+        expect(screen.getByText(/Send message!/)).toBeDisabled()
+    })
+    it('button have style background color green', () => {
+        render(<Button />)
+        expect(screen.getByText(/Send message!/)).toHaveStyle({ backgroundColor: '#4cc49c'})
+    })
+    // it('button click with userEvent', () => {
+    //     const mockHandler = jest.fn();
+    //     render (<Button handleClick={mockHandler} />)
+    //     userEvent.click(screen.getByText('Send message!'))
+    //     expect(mockHandler).toBeCalledTimes(1)
+    // })
+    // it('button async click', async () => {
+    //     const mockHandler = jest.fn();
+    //     render (<Button handleClick={() => setTimeout(mockHandler, 1000)} />)
+    //     userEvent.click(screen.getByText(/Send message!/))
+    //     await waitFor(() => expect(mockHandler).toHaveBeenCalledTimes(1), {
+    //         timeout:1100,
+    //     })
+    // })
+    
 })
