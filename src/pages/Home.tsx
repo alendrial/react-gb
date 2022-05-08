162f9ec081927d7e4e86582ed83ad56602818f68
Alendrial <mashkinmv94@gmail.com>
2022-05-08 18:24:37 +0700

    fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import homeImg from './assets/react-logo-vector.svg'
+import homeImg from './assets/reactLogo.svg'
 
 export const Home: FC = () => {
+    console.log(homeImg)
     return (
         <>
         <h2 style={{ textAlign: 'center' }}>Home</h2>
-        <img src={homeImg} style={{ width: '50%'}} alt="" />
+        <img src={homeImg} style={{ width: '50%', height: '50%' }} alt="" />
         </>
     )
 }
