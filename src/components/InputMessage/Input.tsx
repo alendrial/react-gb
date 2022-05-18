155aa55f7a26ac87342dad520db5723ab61efcd1
Alendrial <mashkinmv94@gmail.com>
2022-05-18 23:30:30 +0700

    fixes

--- src/components/InputMessage/Input.tsx
+++ src/components/InputMessage/Input.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import './Input.scss';
 
 interface InputProps {
   value: string;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './index.css';
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { App } from './App';
+import './index.scss';
 
 const container = document.getElementById('root');
 
